7a44e3956b8242c257938fac2d17ae9250cd816d
eoller <Egorpyp@gmail.com>
2019-08-24 12:46:48 +0200

    Initial Commit

---
+++ TableViewListProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewListProject
+//
+//  Created by Jegor Melnik on 24/08/2019.
+//  Copyright © 2019 Jegor Melnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
